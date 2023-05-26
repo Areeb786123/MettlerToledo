eacc3013f580e751f5a5ae5ee9d75e3aa0be69a7
areeb786 <areebrockzz786@gmail.com>
2023-05-26 16:22:51 +0530

    mkmkm

--- app/src/main/java/com/areeb/mettlertoledo/MainActivity.kt
+++ app/src/main/java/com/areeb/mettlertoledo/MainActivity.kt
@@ -6,72 +6,108 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.hardware.usb.UsbDevice
+import android.hardware.usb.UsbDeviceConnection
 import android.hardware.usb.UsbManager
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.areeb.mettlertoledo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var _binding: ActivityMainBinding
     private val binding get() = _binding
-    private lateinit var usbManager: UsbManager
-    private lateinit var usbReceiver: BroadcastReceiver
 
     companion object {
         private const val ACTION_USB_PERMISSION = "com.areeb.mettlertoledo.USB_PERMISSION"
     }
 
+    private lateinit var usbManager: UsbManager
+    private var usbDevice: UsbDevice? = null
+    private lateinit var usbConnection: UsbDeviceConnection
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = _binding.root
-        setContentView(view)
+        setContentView(_binding.root)
 
         usbManager = getSystemService(Context.USB_SERVICE) as UsbManager
 
-        usbReceiver = object : BroadcastReceiver() {
+        // Register a broadcast receiver to handle USB permission request
+        val usbPermissionReceiver = object : BroadcastReceiver() {
             override fun onReceive(context: Context, intent: Intent) {
-                if (intent.action == ACTION_USB_PERMISSION) {
-                    val granted = intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)
-                    val device: UsbDevice? = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE)
-
-                    if (granted && device != null) {
-                        // Permission granted, read data from the USB device
-                        val data = readDataFromUsbDevice(device)
-                        binding.wightTextView.text = data
-                        Log.e("dataXXX", data)
+                val action = intent.action
+                if (ACTION_USB_PERMISSION == action) {
+                    synchronized(this) {
+                        val device = intent.getParcelableExtra<UsbDevice>(UsbManager.EXTRA_DEVICE)
+                        if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
+                            if (device != null) {
+                                // Permission granted, open the connection
+                                usbDevice = device
+                                openConnection()
+                            }
+                        } else {
+                            // Permission denied
+                            Toast.makeText(
+                                this@MainActivity,
+                                "Permission denied for USB device",
+                                Toast.LENGTH_SHORT,
+                            ).show()
+                        }
                     }
                 }
             }
         }
 
-        // Register the USB receiver to listen for USB permission broadcasts
-        val filter = IntentFilter(ACTION_USB_PERMISSION)
-        registerReceiver(usbReceiver, filter)
+        // Register the broadcast receiver to receive USB permission events
+        val permissionFilter = IntentFilter(ACTION_USB_PERMISSION)
+        registerReceiver(usbPermissionReceiver, permissionFilter)
 
-        // Request permission to access the USB device
-        val usbDevices: HashMap<String, UsbDevice>? = usbManager.deviceList
-        usbDevices?.values?.forEach { device ->
+        // Request permission for the USB device
+        val devices = usbManager.deviceList.values
+        if (devices.isNotEmpty()) {
+            usbDevice = devices.first()
             val permissionIntent =
                 PendingIntent.getBroadcast(this, 0, Intent(ACTION_USB_PERMISSION), 0)
-            usbManager.requestPermission(device, permissionIntent)
+            usbManager.requestPermission(usbDevice, permissionIntent)
+        } else {
+            // No USB devices found
+            Toast.makeText(this, "No USB devices found", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun openConnection() {
+        usbConnection = usbManager.openDevice(usbDevice)
+        if (usbConnection != null && usbDevice != null) {
+            // Claim the interface
+            val usbInterface = usbDevice!!.getInterface(0)
+            usbConnection.claimInterface(usbInterface, true)
+
+            // Configure the interface and endpoints
+            val endpoint = usbInterface.getEndpoint(0)
+            // endpoint may need to be determined based on your device's specifications
+
+            // Read data from the endpoint
+            val buffer = ByteArray(endpoint.maxPacketSize)
+            val bytesRead = usbConnection.bulkTransfer(endpoint, buffer, buffer.size, 0)
+
+            if (bytesRead > 0) {
+                // Process the received data
+                val receivedData = String(buffer, 0, bytesRead)
+                // TODO: Handle the received data
+                Log.e("jao", receivedData)
+                binding.wightTextView.text = receivedData
+            }
         }
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        unregisterReceiver(usbReceiver)
-    }
 
-    private fun readDataFromUsbDevice(device: UsbDevice): String {
-        // Implement your code to read data from the USB device here
-        // Return the data as a String
-        // Example: return "Data read from USB device"
-        // Replace this example with your actual code to read data from the USB device
-
-        // Simulating data read for demonstration purposes
-        val simulatedData = device.toString()
-        return simulatedData
+        // Release the USB connection
+        if (usbConnection != null) {
+            usbConnection.releaseInterface(usbDevice!!.getInterface(0))
+            usbConnection.close()
+        }
     }
 }
---
+++ app/src/main/java/com/areeb/mettlertoledo/ScaleManager.kt
@@ -0,0 +1,5 @@
+package com.areeb.mettlertoledo
+
+import android.app.Activity
+
+class ScaleManager(private val activity: Activity)
